38d8bcf6c597b5292ae76e16de3afdfa62c3c644
北䑓如法 <algebraicallyClosedField@gmail.com>
2016-03-01 19:49:36 +0900

    The first commit

---
+++ Question/QuestionBookmarkManager.swift
@@ -0,0 +1,15 @@
+//
+//  QuestionBookmarkManager.swift
+//  Question
+//
+//  Created by 北䑓 如法 on 16/3/1.
+//  Copyright © 2016年 北䑓 如法. All rights reserved.
+//
+
+import Foundation
+import OAuthSwift
+import KeychainAccess
+
+class QuestionBookmarkManager {
+    
+}
---
+++ QuestionTests/QuestionTests.swift
@@ -0,0 +1,34 @@
+//
+//  QuestionTests.swift
+//  QuestionTests
+//
+//  Created by 北䑓 如法 on 16/3/1.
+//  Copyright © 2016年 北䑓 如法. All rights reserved.
+//
+
+import XCTest
+@testable import Question
+
+class QuestionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
