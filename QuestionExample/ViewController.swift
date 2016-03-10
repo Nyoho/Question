456af39921300c1b6cc53354d0930edb2459fab6
北䑓如法 <algebraicallyClosedField@gmail.com>
2016-03-10 22:22:48 +0900

    Add a sample project named QuestionExample

---
+++ QuestionExample/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  QuestionExample
+//
+//  Created by 北䑓 如法 on 16/3/8.
+//  Copyright © 2016年 北䑓 如法. All rights reserved.
+//
+
+import Cocoa
+import Question
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        QuestionBookmarkManager.sharedManager.setConsumerKey("", consumerSecret: "")
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
