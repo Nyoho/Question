efae6d3f3f7436c2b159875d75e26996c422f345
北䑓如法 <algebraicallyClosedField@gmail.com>
2018-04-07 12:40:41 +0900

    Add a view controller of WKWebView for authentification

---
+++ Question/QuestionAuthViewController.swift
@@ -0,0 +1,18 @@
+//
+//  QuestionAuthViewController.swift
+//  Question
+//
+//  Created by 北䑓 如法 on 18/4/8.
+//  Copyright © 2018年 北䑓 如法. All rights reserved.
+//
+
+import Cocoa
+
+class QuestionAuthViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
