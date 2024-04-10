dc132ac6c6a79d590b0726e62482a84db1c7922a
北䑓如法 <algebraicallyClosedField@gmail.com>
2024-04-11 07:27:40 +0900

    Add a small test

--- Tests/QuestionTests/QuestionTests.swift
+++ Tests/QuestionTests/QuestionTests.swift
@@ -6,7 +6,7 @@ final class QuestionTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(Question().text, "Hello, World!")
+        XCTAssertNotNil(QuestionBookmarkManager.shared)
     }
 
     static var allTests = [
